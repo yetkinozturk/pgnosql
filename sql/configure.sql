31463cfe92c07245d29fb7a50d4dacb417dd53e1
orges <yetkin85@gmail.com>
2014-04-23 21:08:25 +0300

    initial sqls, README Update

---
+++ sql/configure.sql
@@ -0,0 +1,19 @@
+CREATE EXTENSION hstore;
+CREATE EXTENSION pgcrypto;
+CREATE EXTENSION pg_trgm;
+
+
+CREATE TABLE kvstore (
+	  id serial PRIMARY KEY,
+	  key varchar(128) UNIQUE,
+	  key_hash bytea UNIQUE,
+	  value text,
+	  entry_date DATE,
+	  expire_date DATE,
+);
+
+
+CREATE UNIQUE INDEX kvstore_eq_key ON kvstore USING btree (key);
+CREATE UNIQUE INDEX kvstore_hashedkey ON kvstore USING btree (key_hash);
+CREATE INDEX kvstore_likekey ON kvstore USING GIST (key gist_trgm_ops);
+
---
+++ sql/delete.sql
@@ -0,0 +1,3 @@
+DROP DATABASE pgnosql;
+DROP ROLE pgnosql;
+
